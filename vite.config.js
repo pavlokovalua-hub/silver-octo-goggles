a6f189c162c271063839021a8d0901d16e578b71
Pavlo Koval <pavlok@ilmakiage.com>
2026-06-04 13:52:46 +0200

    extend forehead extension zone, add movving to the head shake

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
+const hasCertificates = fs.existsSync('key.pem') && fs.existsSync('cert.pem');
+
 export default defineConfig({
   plugins: [react()],
   base: '/silver-octo-goggles/',
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: {
+    https: hasCertificates ? {
       key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
-    },
+    } : false,
   },
 })
